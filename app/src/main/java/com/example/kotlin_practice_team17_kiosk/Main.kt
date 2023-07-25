075eb72b2f990eca2a9807b7af07143fb5a5eeb9
LeeChoongHwan <choo901@naver.com>
2023-07-25 09:39:37 +0900

    add Side Class

--- app/src/main/java/com/example/kotlin_practice_team17_kiosk/Main.kt
+++ app/src/main/java/com/example/kotlin_practice_team17_kiosk/Main.kt
@@ -1,7 +1,4 @@
 package com.example.kotlin_practice_team17_kiosk
-
-import com.example.kotlin_practice_team17_kiosk.side.CheezeStick
-
 fun main() {
 
 }
--- app/src/main/java/com/example/kotlin_practice_team17_kiosk/side/MacNugget.kt
+++ app/src/main/java/com/example/kotlin_practice_team17_kiosk/side/MacNugget.kt
@@ -1,4 +1,11 @@
 package com.example.kotlin_practice_team17_kiosk.side
 
-class MacNugget {
+class MacNugget : Side() {
+    override var name = "맥너겟"
+    override var price = 3300
+    override var description = "163kcal/4조각"
+
+    override fun printMenu() {
+        println("3. ${name} | ${price} | ${description}")
+    }
 }
