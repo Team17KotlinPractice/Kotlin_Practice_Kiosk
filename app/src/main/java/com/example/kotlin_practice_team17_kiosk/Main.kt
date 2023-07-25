4f55ba7bc50e11ccfeeec429096dfb643e201d6c
shyr0809 <139097952+shyr0809@users.noreply.github.com>
2023-07-25 20:24:48 +0900

    ;.

--- app/src/main/java/com/example/kotlin_practice_team17_kiosk/Main.kt
+++ app/src/main/java/com/example/kotlin_practice_team17_kiosk/Main.kt
@@ -2,6 +2,27 @@ package com.example.kotlin_practice_team17_kiosk
 
 import com.example.kotlin_practice_team17_kiosk.side.CheezeStick
 
-fun main() {
+import java.lang.NumberFormatException
 
+var menuPrinter = MenuPrinter()
+fun main() {
+    while(true) {
+        menuPrinter.printTotalMenu()
+        try {  var totalMenuChoice = readLine()?.toInt()
+            when (totalMenuChoice) {
+                1 -> {}
+                2 -> menuPrinter.printDrinkMenu()
+                3 -> menuPrinter.printBurgerMenu()
+                4 -> {}
+                5 -> break
+                else -> {
+                    println("올바른 번호를 입력하세요.")
+                }
+            }
+
+        }catch (e: NumberFormatException){
+            println("숫자를 입력해주세요")
+        }
+
+    }
 }
--- app/src/main/java/com/example/kotlin_practice_team17_kiosk/MenuPrinter.kt
+++ app/src/main/java/com/example/kotlin_practice_team17_kiosk/MenuPrinter.kt
@@ -1,5 +1,36 @@
 package com.example.kotlin_practice_team17_kiosk
 
+import com.example.kotlin_practice_team17_kiosk.side.CheezeStick
+import com.example.kotlin_practice_team17_kiosk.side.FrenchFries
+import com.example.kotlin_practice_team17_kiosk.side.MacNugget
+import com.example.kotlin_practice_team17_kiosk.side.MacWing
+import com.example.kotlin_practice_team17_kiosk.side.drink.Coke
+import com.example.kotlin_practice_team17_kiosk.side.drink.Fanta
+import com.example.kotlin_practice_team17_kiosk.side.drink.Sprite
+import com.example.kotlin_practice_team17_kiosk.side.drink.ZeroCoke
+
 class MenuPrinter {
+    fun printTotalMenu() {
+        println("-----전체 메뉴를 출력합니다.-----")
+        println("1. 버거")
+        println("2. 음료")
+        println("3. 사이드")
+        println("4. 디저트")
+        println("5. 종료")
+    }
+    fun printDrinkMenu() {
+
+        var coke = Coke()
+        var fanta = Fanta()
+        var sprite = Sprite()
+        var zeroCoke = ZeroCoke()
+
+        coke.printDrink()
+        sprite.printDrink()
+        fanta.printDrink()
+        zeroCoke.printDrink()
+        println("5. 뒤로가기")
+    }
+
 
 }
