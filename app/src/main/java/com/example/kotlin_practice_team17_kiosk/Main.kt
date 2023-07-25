28e7ef8702e756532aba91bdc796124661778701
LeeChoongHwan <choo901@naver.com>
2023-07-25 22:50:49 +0900

    edit main

--- app/src/main/java/com/example/kotlin_practice_team17_kiosk/Main.kt
+++ app/src/main/java/com/example/kotlin_practice_team17_kiosk/Main.kt
@@ -12,9 +12,19 @@ fun main() {
                 1 -> {}
                 2 -> {
                     menuPrinter.printDrinkMenu()
+                    var drinkMenuChoice = readLine()?.toInt()
+                    menuPrinter.printPurchaseOrNot()
                 }
-                3 -> menuPrinter.printSideMenu()
-                4 -> menuPrinter.printDessertMenu()
+                3 -> {
+                    menuPrinter.printSideMenu()
+                    var sideMenuChoice = readLine()?.toInt()
+                    menuPrinter.printPurchaseOrNot()
+                }
+                4 -> {
+                    menuPrinter.printDessertMenu()
+                    var dessertMenuChoice = readLine()?.toInt()
+                    menuPrinter.printPurchaseOrNot()
+                }
                 5 -> break
                 else -> {
                     println("올바른 번호를 입력하세요.")
