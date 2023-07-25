981c27267fd8277f105ee2d426e836de53a00357
seunghoon Lee <hara9052@naver.com>
2023-07-25 14:48:05 +0900

    예ì외처리

--- app/src/main/java/com/example/kotlin_practice_team17_kiosk/Main.kt
+++ app/src/main/java/com/example/kotlin_practice_team17_kiosk/Main.kt
@@ -1,19 +1,26 @@
 package com.example.kotlin_practice_team17_kiosk
 
+import java.lang.NumberFormatException
+
 var menuPrinter = MenuPrinter()
 fun main() {
     while(true) {
         menuPrinter.printTotalMenu()
-        var totalMenuChoice = readLine()?.toInt()
-        when (totalMenuChoice) {
-            1 -> {}
-            2 -> menuPrinter.printDrinkMenu()
-            3 -> menuPrinter.printSideMenu()
-            4 -> {}
-            5 -> break
-            else -> {
-                println("올바른 번호를 입력하세요.")
+        try {  var totalMenuChoice = readLine()?.toInt()
+            when (totalMenuChoice) {
+                1 -> {}
+                2 -> menuPrinter.printDrinkMenu()
+                3 -> menuPrinter.printSideMenu()
+                4 -> {}
+                5 -> break
+                else -> {
+                    println("올바른 번호를 입력하세요.")
+                }
             }
+
+        }catch (e: NumberFormatException){
+            println("숫자를 입력해주세요")
         }
+
     }
 }
