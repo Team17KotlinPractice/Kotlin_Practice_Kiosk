ac42ec4d16f48466a022ff8473246d5ee1d3d1a7
shyr0809 <139097952+shyr0809@users.noreply.github.com>
2023-07-26 09:44:33 +0900

    버거

--- app/src/main/java/com/example/kotlin_practice_team17_kiosk/Main.kt
+++ app/src/main/java/com/example/kotlin_practice_team17_kiosk/Main.kt
@@ -1,6 +1,4 @@
-package com.example.kotlin_practice_team17_kiosk
-
-
+import com.example.kotlin_practice_team17_kiosk.MenuPrinter
 
 import java.lang.NumberFormatException
 
@@ -10,16 +8,18 @@ fun main() {
         menuPrinter.printTotalMenu()
         try {  var totalMenuChoice = readLine()?.toInt()
             when (totalMenuChoice) {
-                1 -> menuPrinter.printBurgerMenu()
-                2 -> menuPrinter.printDrinkMenu()
-                3 -> {}
-                4 -> {}
+
+                1 -> {}
+                2 -> {
+                    menuPrinter.printDrinkMenu()
+                }
+                3 -> menuPrinter.printSideMenu()
+                4 -> menuPrinter.printDessertMenu()
                 5 -> break
                 else -> {
-                    println("올바른 번호를 입력하세요")
+                    println("올바른 번호를 입력하세요.")
                 }
             }
-
         }catch (e: NumberFormatException){
             println("숫자를 입력해주세요")
         }
--- app/src/main/java/com/example/kotlin_practice_team17_kiosk/MenuPrinter.kt
+++ app/src/main/java/com/example/kotlin_practice_team17_kiosk/MenuPrinter.kt
@@ -1,10 +1,18 @@
 package com.example.kotlin_practice_team17_kiosk
 
+
 import com.example.kotlin_practice_team17_kiosk.burger.BigMac
 import com.example.kotlin_practice_team17_kiosk.burger.DoubleQuaterPoundCheeze
 import com.example.kotlin_practice_team17_kiosk.burger.MacCrispy
 import com.example.kotlin_practice_team17_kiosk.burger.Shanghai
-
+import com.example.kotlin_practice_team17_kiosk.dessert.ApplePie
+import com.example.kotlin_practice_team17_kiosk.dessert.Chiller
+import com.example.kotlin_practice_team17_kiosk.dessert.Coleslaw
+import com.example.kotlin_practice_team17_kiosk.dessert.MacFlurry
+import com.example.kotlin_practice_team17_kiosk.side.CheezeStick
+import com.example.kotlin_practice_team17_kiosk.side.FrenchFries
+import com.example.kotlin_practice_team17_kiosk.side.MacNugget
+import com.example.kotlin_practice_team17_kiosk.side.MacWing
 import com.example.kotlin_practice_team17_kiosk.side.drink.Coke
 import com.example.kotlin_practice_team17_kiosk.side.drink.Fanta
 import com.example.kotlin_practice_team17_kiosk.side.drink.Sprite
@@ -32,6 +40,7 @@ class MenuPrinter {
         zeroCoke.printDrink()
         println("5. 뒤로가기")
     }
+
     fun printBurgerMenu() {
 
         var bigMac = BigMac()
@@ -43,6 +52,33 @@ class MenuPrinter {
         doubleQuaterPoundCheeze.printBurger()
         macCrispy.printBurger()
         shanghai.printBurger()
+        println("5. 뒤로가기")
+    }
+
+
+    fun printSideMenu() {
+        var cheezeStick = CheezeStick()
+        var frenchFries = FrenchFries()
+        var macNugget = MacNugget()
+        var macWing = MacWing()
+
+        cheezeStick.printMenu()
+        frenchFries.printMenu()
+        macNugget.printMenu()
+        macWing.printMenu()
+        println("5. 뒤로가기")
+    }
+    fun printDessertMenu() {
+        var applePie = ApplePie()
+        var chiller = Chiller()
+        var coleslaw = Coleslaw()
+        var macFlurry = MacFlurry()
+
+
+        applePie.printDessert()
+        chiller.printDessert()
+        coleslaw.printDessert()
+        macFlurry.printDessert()
         println("5. 뒤로가기")
     }
 
